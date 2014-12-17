2492cb573ae4ae42f49e25269cefc9d670f4fcd0
Rollie Ma <rollie.ma@gmail.com>
2014-12-16 23:32:20 -0800

    Added an empty Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/*
+ * varius
+ *
+ * Node.js and Socket.io powered presentation app
+ * https://github.com/waltzofpearls/varius
+ *
+ * Copyright (c) 2014 Topbass Labs (topbasslabs.com)
+ * Author Waltz.of.Pearls <rollie@topbasslabs.com, rollie.ma@gmail.com>
+ */
