2fd7ff9182c4cd6c01743500f7fe88b1336a7b47
Rollie Ma <rollie.ma@gmail.com>
2014-12-18 23:57:25 -0800

    Added less build task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,3 +7,34 @@
  * Copyright (c) 2014 Topbass Labs (topbasslabs.com)
  * Author Waltz.of.Pearls <rollie@topbasslabs.com, rollie.ma@gmail.com>
  */
+
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        less: {
+            testing: {
+                options: {
+                    strictMath: true,
+                    compress: false
+                },
+                src: 'public/stylesheets/style.less',
+                dest: 'public/stylesheets/style.css'
+            },
+            production: {
+                options: {
+                    strictMath: true,
+                    cleancss: true
+                },
+                src: 'public/stylesheets/style.less',
+                dest: 'public/stylesheets/style.css'
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+    require('time-grunt')(grunt);
+
+    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('default', ['build']);
+};
